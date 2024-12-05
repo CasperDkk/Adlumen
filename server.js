0a3de2460f61ebae89ca72d4b17361bcf0507b89
CasperDkk <daniel.kamau8@gmail.com>
2024-12-06 01:38:51 +0300

    server.js middleware and logging logic added

--- server.js
+++ server.js
@@ -3,22 +3,25 @@ const bodyParser = require('body-parser');
 const adRoutes = require('./routes/adRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const { connectDB } = require('./config/db-config');
-const logger = require ('./utils/logger')
+const logger = require('./utils/logger'); 
 
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(express.json());  
 
 // Routes
-app.use('/api/ads', adRoutes);
+app.use('/api/ads', adRoutes); // API routes to have prefix /api
 app.use('/api/payments', paymentRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
-  await connectDB(); // Initialize DB connection
-  console.log(`Server running on http://localhost:${PORT}`);
+  try {
+    await connectDB(); 
+    logger.info(`Server running on http://localhost:${PORT}`); 
+  } catch (error) {
+    logger.error('Error connecting to the database:', error);
+    process.exit(1); // Exit process if DB connection fails
+  }
 });
-
-
