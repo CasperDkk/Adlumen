02aec5b6574705d9e45d8371a272567a861f701f
CasperDkk <daniel.kamau8@gmail.com>
2024-12-06 11:40:27 +0300

    lnd-config changes

--- config/lnd-config.js
+++ config/lnd-config.js
@@ -3,7 +3,7 @@ const { authenticatedLndGrpc } = require('ln-service');
 const config = {
   cert: process.env.LND_CERT_PATH,  // Path to your LND TLS certificate
   macaroon: process.env.LND_MACAROON_PATH,  // Path to your macaroon
-  socket: process.env.LND_SOCKET || '127.0.0.1:10009',  // Default socket
+  socket: process.env.LND_SOCKET 
 };
 
 const { lnd } = authenticatedLndGrpc(config);
